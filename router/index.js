2528c9fc32a3c8cc6ef3972d59e7e5d5242b7f94
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-13 18:07:02 +0900

    Fix : 로그인 연결

--- router/index.js
+++ router/index.js
@@ -1,11 +1,15 @@
-var express = require('express')
-var app = express()
-var router = express.Router()
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
-var registerRouter = require('./register/register')
-var loginRouter = require('./login/login')
+// MySQL 연결 설정
+const db = require('./config/db');
 
-router.use('/register', registerRouter)
-router.use('/login', loginRouter)
+const router = require('./router/index');
+app.use(cors());
+app.use(router);
 
-module.exports = router
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
