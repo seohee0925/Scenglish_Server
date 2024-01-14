cb355559b844bd44c94449082f21aabcc46a1bbc
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-14 14:38:15 +0900

    Feat : 영화 정보 반영

--- router/index.js
+++ router/index.js
@@ -4,8 +4,10 @@ var router = express.Router()
 
 var registerRouter = require('./register/register')
 var loginRouter = require('./login/login')
+var quotesRouter = require('./quotes/quotes')
 
 router.use('/register', registerRouter)
 router.use('/login', loginRouter)
+router.use('/quotes', quotesRouter)
 
 module.exports = router
