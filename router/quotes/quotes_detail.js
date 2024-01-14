5b403a419ad28081a7023538261ba2eebf58c317
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-15 02:15:46 +0900

    Feat : 영화 페이지 조회

--- router/quotes/quotes_detail.js
+++ router/quotes/quotes_detail.js
@@ -6,19 +6,22 @@ router.use(express.json());
 
 // quotes/getMovieInfo
 router.get('/getMovieInfo', (req, res) => {
-    const movieTitle = req.query.movieTitle; // movie title을 받아서 저장 (url과 변수명 일치)
+    const contentsId = req.query.contentsId; // contents Id를 요청받아서 저장 (url과 변수명 일치)
+    const query = `
+        SELECT contents.*, quotes.*
+        FROM contents
+        INNER JOIN quotes ON contents.movie = quotes.movie
+        WHERE contents_id = ?
+    `;
 
-    const query = "SELECT * FROM quotes WHERE movie = ?";
-
-    db.query(query, [movieTitle], (error, results, fields) => {
+    db.query(query, [contentsId], (error, results, fields) => {
         if(error) {
             console.error('Error fetching movie info:', error);
             res.status(500).json({ error: 'Internal Server error' });
         } else {
             if (results.length > 0) { // 영화 정보 응답
                 res.status(200).json(results);
-            }
-            else { // 일치하는 영화가 없을 때
+            } else { // 일치하는 영화가 없을 때
                 res.status(404).json({ error: 'Movie not found' })
             }
         }
