915ba801f01674fae267f9f70542c90f0f9df08c
윤서희 <appleid21@sookmyung.ac.kr>
2024-01-14 16:23:21 +0900

    Feat : 시청 정보 저장

--- router/index.js
+++ router/index.js
@@ -5,9 +5,11 @@ var router = express.Router()
 var registerRouter = require('./register/register')
 var loginRouter = require('./login/login')
 var quotesRouter = require('./quotes/quotes')
+// var watchedRouter = require('./quotes/quotes_user')/
 
 router.use('/register', registerRouter)
 router.use('/login', loginRouter)
 router.use('/quotes', quotesRouter)
+// router.use('/quotes', watchedRouter)
 
 module.exports = router
